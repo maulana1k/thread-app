fcdf995a41e2341b87da682e2cfe0f64aff6d90d
maulana1k <mm930199@gmail.com>
2025-11-30 15:34:01 +0700

    fix: add Suspense component for useQueryParam

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,13 @@
+import { Spinner } from "@/components/ui/spinner";
 import { RegistrationFlow } from "@/features/auth/components/registration-flow";
+import { Suspense } from "react";
 
 export default function RegisterPage() {
   return (
     <div className="min-h-screen flex items-center justify-center p-4">
-      <RegistrationFlow />
+      <Suspense fallback={<div><Spinner /></div>}>
+        <RegistrationFlow />
+      </Suspense>
     </div>
   );
 }
